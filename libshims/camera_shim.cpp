9bbb01d44e130a37d0048e13020cd1f8c76511a8
Olivier Karasangabo <olivier@olivierk.eu>
2017-01-08 13:53:10 +0100

    Initial commit

---
+++ libshims/camera_shim.cpp
@@ -0,0 +1,3 @@
+// TODO: Fix this properly
+const char *_ZN7android11MPEG4Writer20setRecordingModeDataEi = "setRecordingModeData";
+const char *_ZN7android11MPEG4Writer13setAuthorDataEi = "setAuthorData";
---
+++ libshims/libMP4Writer_native_shim.cpp
@@ -0,0 +1,9 @@
+// TODO: Fix this properly
+const char *_ZN7android25MEDIA_MIMETYPE_VIDEO_WMV1E = "video/wmv1";
+const char *_ZN7android25MEDIA_MIMETYPE_VIDEO_WMV2E = "video/wmv2";
+const char *_ZN7android25MEDIA_MIMETYPE_VIDEO_WMV3E = "video/wmv3";
+const char *_ZN7android25MEDIA_MIMETYPE_VIDEO_WVC1E = "video/wvc1";
+const char *_ZN7android28MEDIA_MIMETYPE_CONTAINER_MKVE = "video/mkv";
+const char *_ZN7android28MEDIA_MIMETYPE_CONTAINER_ASFE = "video/asf";
+const char *_ZN7android14MediaExtractor6CreateERKNS_2spINS_10DataSourceEEEPKc;
+const char *_ZN7android10DataSource13CreateFromURIERKNS_2spINS_17IMediaHTTPServiceEEEPKcPKNS_11KeyedVectorINS_7String8ES9_EEPS9_PNS_8HTTPBaseE;
